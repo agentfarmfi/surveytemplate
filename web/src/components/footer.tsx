39c9fdb08cbde3cf6ca2d2057b7d424e995a4cdb
Ville Eloranta <ville.eloranta@aalto.fi>
2025-02-26 23:07:22 +0200

    b0.1

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -2,12 +2,12 @@ import { Logo } from '@/components/icons';
 export default function Footer() {
   return (
     <footer className='container mx-auto max-w-7xl py-24 px-12'>
-      <div className='container mx-auto flex justify-center'>
+      <div className='container mx-auto flex flex-col items-center justify-center'>
         <span className='text-center'>
           <Logo />
         </span>
+        <p className='mt-4 text-center'>Beta 0.1 / Aalto University</p>
       </div>
-
     </footer>
   );
 }
